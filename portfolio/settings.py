746c53da9e3db4e49aa21ca7f56163860cad3fcb
Muppala Chethan Raju <muppalachethan123@gmail.com>
2025-03-04 21:30:45 +0530

    all done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-(#f$s@b8bec$w=n0)3gd+^921*e#rea9%59hjwk8*=mse^b0nd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
