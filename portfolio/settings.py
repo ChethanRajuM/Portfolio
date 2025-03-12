e3afc363ff97d658d9da225ca187848aa8be0735
Muppala Chethan Raju <muppalachethan123@gmail.com>
2025-03-12 16:09:00 +0530

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+   
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [STATICFILES_DIRS]  # Ensure project-level static folder is included]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Where collectstatic will store files
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,6 +19,12 @@ from django.contrib import admin
 from django.urls import path
 from portfolio import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
     path("",views.Portfolio_view),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
